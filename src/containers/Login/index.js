713605c3ab3b3d72e392d854f374aeb3928aedf7
anshuul <anshultated10@gmail.com>
2024-03-11 14:14:54 +0530

    done

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,94 +1,106 @@
-import React from 'react';
-import { SvgIcon } from '../../components/common';
-import { Link } from 'react-router-dom';
-import { Button, Divider } from 'antd';
-import { Form, Input } from 'antd';
-import './index.scss'
+import React from "react";
+import { SvgIcon } from "../../components/common";
+import { Link } from "react-router-dom";
+import { Button, Divider } from "antd";
+import { Form, Input } from "antd";
+import "./index.scss";
 
-import logoImage from '../../assets/images/logo-light.svg';
-import logoImage1 from '../../assets/images/logo.svg';
-import AwesomeImg from '../../assets/images/tp-left.png';
-import VideoOne from '../../assets/video/bg_auth.mp4';
+import logoImage from "../../assets/images/logo-light.svg";
+import logoImage1 from "../../assets/images/logo.svg";
+import AwesomeImg from "../../assets/images/tp-left.png";
+import VideoOne from "../../assets/video/bg_auth.mp4";
 
 const Login = () => {
-    return (
-        <div className='auth-wrapper'>
-            <div className='auth-left'>
-                <div className='authleft-inner'>
-                    <div>
-                        <div className='dextop-logo'>
-                            <Link to='/'><img src={logoImage} alt={logoImage} /></Link>
-                        </div>
-                        <h1>
-                            Discover <br />
-                            Your City
-                        </h1>
-                    </div>
-                    <div>
-                        <div className='awse-img'><img src={AwesomeImg} alt="Awesome" /></div>
-                        <h3>Awesome!</h3>
-                        <p>
-                            Awesome! It’s easy to use and very handy. It helps a lot to save more. Worth every penny
-                        </p>
-                        <div className='left-name'>Nikol</div>
-                    </div>
-                </div>
-                <video playsInline autoPlay muted loop className="banner-video">
-                    <source src={VideoOne} />
-                </video>
+  return (
+    <div className="auth-wrapper">
+      <div className="auth-left">
+        <div className="authleft-inner">
+          <div>
+            <div className="dextop-logo">
+              <Link to="/">
+                <img src={logoImage} alt={logoImage} />
+              </Link>
             </div>
-            <div className='auth-right'>
-                <div className='authright-inner'>
-                    <div className='right-upper'>
-                        <div className='mobile-logo'>
-                            <Link to='/'><img src={logoImage1} alt={logoImage1} /></Link>
-                        </div>
-                        <div className='upper-inner'>
-                            <h2>Login</h2>
-                            <div className='right-head'>
-                                <span>Don't have an account?</span>
-                                <Link to='/signup' className='auth-upper-btn ant-btn'>
-                                    Sign Up
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='right-form'>
-                        <Form
-                            name="basic"
-                            autoComplete="off"
-                            layout="vertical"
-                        >
-                            <Form.Item label="Email">
-                                <Input prefix={<SvgIcon name='email-icon' viewbox='0 0 30 30' />} placeholder='Enter' />
-                            </Form.Item>
-                            <Form.Item className='m-0 py-3'>
-                                <Link to='/enter-otp'>
-                                    <Button type="primary" htmlType="submit" block>
-                                        Log In
-                                    </Button>
-                                </Link>
-                            </Form.Item>
-                        </Form>
-                    </div>
-                    <div className='auth-right-footer'>
-                        <Divider>Or Log In with Email</Divider>
-                        <ul>
-                            <li>
-                                <Button><SvgIcon name='apple' viewbox='0 0 22.982 28.234' /></Button>
-                            </li>
-                            <li>
-                                <Button><SvgIcon name='google' viewbox='0 0 28.688 29.243' /></Button>
-                            </li>
-                            <li>
-                                <Button><SvgIcon name='facebook' viewbox='0 0 8.106 17.344' /></Button>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+            <h1>
+              Discover <br />
+              Your City
+            </h1>
+          </div>
+          <div>
+            <div className="awse-img">
+              <img src={AwesomeImg} alt="Awesome" />
             </div>
+            <h3>Awesome!</h3>
+            <p>
+              Awesome! It’s easy to use and very handy. It helps a lot to save
+              more. Worth every penny
+            </p>
+            <div className="left-name">Nikol</div>
+          </div>
         </div>
-    )
-}
+        <video playsInline autoPlay muted loop className="banner-video">
+          <source src={VideoOne} />
+        </video>
+      </div>
+      <div className="auth-right">
+        <div className="authright-inner">
+          <div className="right-upper">
+            <div className="mobile-logo">
+              <Link to="/">
+                <img src={logoImage1} alt={logoImage1} />
+              </Link>
+            </div>
+            <div className="upper-inner">
+              <h2>Login</h2>
+              <div className="right-head">
+                <span>Don't have an account?</span>
+                <Link to="/signup" className="auth-upper-btn ant-btn">
+                  Sign Up
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="right-form">
+            <Form name="basic" autoComplete="off" layout="vertical">
+              <Form.Item label="Email">
+                <Input
+                  prefix={<SvgIcon name="email-icon" viewbox="0 0 30 30" />}
+                  placeholder="Enter"
+                />
+              </Form.Item>
+              <Form.Item className="m-0 py-3">
+                <Link to="/enter-otp">
+                  <Button type="primary" htmlType="submit" block>
+                    Log In
+                  </Button>
+                </Link>
+              </Form.Item>
+            </Form>
+          </div>
+          <div className="auth-right-footer">
+            <Divider>Or Log In with Email</Divider>
+            <ul>
+              <li>
+                <Button>
+                  <SvgIcon name="apple" viewbox="0 0 22.982 28.234" />
+                </Button>
+              </li>
+              <li>
+                <Button>
+                  <SvgIcon name="google" viewbox="0 0 28.688 29.243" />
+                </Button>
+              </li>
+              <li>
+                <Button>
+                  <SvgIcon name="facebook" viewbox="0 0 8.106 17.344" />
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/containers/ProfileSetting/index.js
+++ src/containers/ProfileSetting/index.js
@@ -1,124 +1,137 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, SvgIcon } from '../../components/common';
-import FavouriteTab from './FavouriteTab';
-import ProfileSettingTab from './ProfileSettingTab';
-import MyCardsTab from './MyCardsTab';
-import MyOffersTab from './MyOffersTab';
-import NotificationSettingTab from './NotificationSettingTab';
-import { Drawer, Button } from 'antd';
-import MediaQuery from 'react-responsive';
-import MyBookingsTab from './MyBookingsTab';
-import './index.scss'
+import React, { useState } from "react";
+import { Container, Row, Col, SvgIcon } from "../../components/common";
+import FavouriteTab from "./FavouriteTab";
+import ProfileSettingTab from "./ProfileSettingTab";
+import MyCardsTab from "./MyCardsTab";
+import MyOffersTab from "./MyOffersTab";
+import NotificationSettingTab from "./NotificationSettingTab";
+import { Drawer, Button } from "antd";
+import MediaQuery from "react-responsive";
+import MyBookingsTab from "./MyBookingsTab";
+import "./index.scss";
 
 const ProfileSetting = () => {
-    const [currentTab, setCurrentTab] = useState('Profile_Setting');
-    const [open, setOpen] = useState(false);
+  const [currentTab, setCurrentTab] = useState("Profile_Setting");
+  const [open, setOpen] = useState(false);
 
-    const showDrawer = () => {
-        setOpen(true);
-    };
+  const showDrawer = () => {
+    setOpen(true);
+  };
 
-    const onClose = () => {
-        setOpen(false);
-    };
-    const tabList = [
-        {
-            name: 'Profile_Setting',
-            icon: <SvgIcon name='profile-setting-icon' viewbox='0 0 9.022 9.736' />,
-            label: 'Profile Setting',
-            content: <ProfileSettingTab />
-        },
-        {
-            name: 'My_Bookings',
-            icon: <SvgIcon name='mybooking' viewbox="0 0 11.026 9.836" />,
-            label: 'My Bookings',
-            content: <MyBookingsTab />
-        },
-        {
-            name: 'Favourite',
-            icon: <SvgIcon name='favourite-icon' viewbox='0 0 10.055 8.961' />,
-            label: 'Wishlist',
-            content: <FavouriteTab />
-        },
-        {
-            name: 'My_Cards',
-            icon: <SvgIcon name='mycards-icon' viewbox='0 0 10.575 7.931' />,
-            label: 'My Cards',
-            content: <MyCardsTab />
-        },
-        {
-            name: 'My_Offers',
-            icon: <SvgIcon name='myoffers-icon' viewbox='0 0 10.083 10.096' />,
-            label: 'My Offers',
-            content: <MyOffersTab />
-        },
-        {
-            name: 'Notification_Setting',
-            icon: <SvgIcon name='notification-icon' viewbox='0 0 8.315 9.262' />,
-            label: 'Notification Setting',
-            content: <NotificationSettingTab />
-        },
-    ];
-    return (
-        <div className='profilesetting-wrapper'>
-            <Container>
-                <Row>
-                    <Col sm='12'>
-                        <div className='profilesetting-main'>
-                            <MediaQuery minWidth={991}>
-                                <div className='profilesetting-left'>
-                                    <ul>
-                                        {
-                                            tabList.map((tab, i) => (
-                                                <li
-                                                    key={i}
-                                                    onClick={() => setCurrentTab(tab.name)}
-                                                    className={(tab.name === currentTab) ? 'active' : ''}>
-                                                    <button><div className='tab-icon'>{tab.icon}</div>{tab.label}</button>
-                                                </li>
-                                            ))
-                                        }
-                                    </ul>
-                                </div>
-                            </MediaQuery>
-                            <MediaQuery maxWidth={991}>
-                                <Button className='profilemenu-btn' type="primary" onClick={showDrawer}>
-                                    <SvgIcon name='menu' viewbox='0 0 24 17.42' />
-                                </Button>
-                            </MediaQuery>
-                            <Drawer className='profilesetting-drawer' width={290} placement="right" onClose={onClose} open={open}>
-                                <div className='profilesetting-left-small' onClick={onClose}>
-                                    <ul>
-                                        {
-                                            tabList.map((tab, i) => (
-                                                <li
-                                                    key={i}
-                                                    onClick={() => setCurrentTab(tab.name)}
-                                                    className={(tab.name === currentTab) ? 'active' : ''}>
-                                                    <button><div className='tab-icon'>{tab.icon}</div>{tab.label}</button>
-                                                </li>
-                                            ))
-                                        }
-                                    </ul>
-                                </div>
-                            </Drawer>
+  const onClose = () => {
+    setOpen(false);
+  };
+  const tabList = [
+    {
+      name: "Profile_Setting",
+      icon: <SvgIcon name="profile-setting-icon" viewbox="0 0 9.022 9.736" />,
+      label: "Profile Setting",
+      content: <ProfileSettingTab />,
+    },
+    {
+      name: "My_Bookings",
+      icon: <SvgIcon name="mybooking" viewbox="0 0 11.026 9.836" />,
+      label: "My Bookings",
+      content: <MyBookingsTab />,
+    },
+    {
+      name: "Favourite",
+      icon: <SvgIcon name="favourite-icon" viewbox="0 0 10.055 8.961" />,
+      label: "Wishlist",
+      content: <FavouriteTab />,
+    },
+    {
+      name: "My_Cards",
+      icon: <SvgIcon name="mycards-icon" viewbox="0 0 10.575 7.931" />,
+      label: "My Cards",
+      content: <MyCardsTab />,
+    },
+    {
+      name: "My_Offers",
+      icon: <SvgIcon name="myoffers-icon" viewbox="0 0 10.083 10.096" />,
+      label: "My Offers",
+      content: <MyOffersTab />,
+    },
+    {
+      name: "Notification_Setting",
+      icon: <SvgIcon name="notification-icon" viewbox="0 0 8.315 9.262" />,
+      label: "Notification Setting",
+      content: <NotificationSettingTab />,
+    },
+  ];
+  return (
+    <div className="profilesetting-wrapper">
+      <Container>
+        <Row>
+          <Col sm="12">
+            <div className="profilesetting-main">
+              <MediaQuery minWidth={991}>
+                <div className="profilesetting-left">
+                  <ul>
+                    {tabList.map((tab, i) => (
+                      <li
+                        key={i}
+                        onClick={() => setCurrentTab(tab.name)}
+                        className={tab.name === currentTab ? "active" : ""}
+                      >
+                        <button>
+                          <div className="tab-icon">{tab.icon}</div>
+                          {tab.label}
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </MediaQuery>
+              <MediaQuery maxWidth={991}>
+                <Button
+                  className="profilemenu-btn"
+                  type="primary"
+                  onClick={showDrawer}
+                >
+                  <SvgIcon name="menu" viewbox="0 0 24 17.42" />
+                </Button>
+              </MediaQuery>
+              <Drawer
+                className="profilesetting-drawer"
+                width={290}
+                placement="right"
+                onClose={onClose}
+                open={open}
+              >
+                <div className="profilesetting-left-small" onClick={onClose}>
+                  <ul>
+                    {tabList.map((tab, i) => (
+                      <li
+                        key={i}
+                        onClick={() => setCurrentTab(tab.name)}
+                        className={tab.name === currentTab ? "active" : ""}
+                      >
+                        <button>
+                          <div className="tab-icon">{tab.icon}</div>
+                          {tab.label}
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </Drawer>
 
-                            <div className='profilesetting-right'>
-                                {tabList.map((tab, i) => {
-                                    if (tab.name === currentTab) {
-                                        return <div key={i}>{tab.content}</div>;
-                                    } else {
-                                        return null;
-                                    }
-                                })
-                                }
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
-}
+              <div className="profilesetting-right">
+                {tabList.map((tab, i) => {
+                  if (tab.name === currentTab) {
+                    return <div key={i}>{tab.content}</div>;
+                  } else {
+                    return null;
+                  }
+                })}
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default ProfileSetting;
